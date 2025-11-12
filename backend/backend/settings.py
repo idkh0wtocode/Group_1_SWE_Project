5f2343366c4089e75950d61c4104677882a256f1
Jesse Bloom <jesse.bloom@ufl.edu>
2025-11-12 14:14:49 -0500

    Connected Django backend to external PostgreSQL cloud DB and Verified LIstings API. Used Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,15 +92,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'magic_mega_marketplace',
-        'USER': 'cen3031',
-        'PASSWORD': 'devpass',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 # Password validation
