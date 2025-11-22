eec5c42c6e7b1d955ef12a0119e1df61c1aee42a
Jesse Bloom <jesse.bloom@ufl.edu>
2025-11-22 11:15:54 -0500

    changed to use christian's db and hopefully allow me to sign in

--- backend/products/models.py
+++ backend/products/models.py
@@ -33,7 +33,7 @@ class Products(models.Model):
         "In Stock": "In Stock",
         "Out of Stock": "Out of Stock",
     }
-    status = models.CharField(choices=PRODUCT_AVAILABILITY, default="In Stock")
+    status = models.CharField(max_length = 20,choices=PRODUCT_AVAILABILITY, default="In Stock")
 
 
     def __str__(self):
